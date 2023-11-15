6dd62774a5c50e004bd75fb1633f3fbb326faa44
0HugoHu <0HugoHu>
2023-11-15 04:43:59 +0000

    Add files

---
+++ ids706_python_template/extract_data.py
@@ -0,0 +1,84 @@
+"""
+extract data from GitHub url
+"""
+
+import requests
+from dotenv import load_dotenv
+import os
+import json
+import base64
+
+
+server_h = os.getenv("SERVER_HOSTNAME")
+access_token = os.getenv("ACCESS_TOKEN")
+FILESTORE_PATH = "dbfs:/FileStore/HugoHu_Project_3"
+headers = {'Authorization': 'Bearer %s' % access_token}
+url = "https://"+server_h+"/api/2.0"
+
+
+def perform_query(path, headers, data={}):
+    session = requests.Session()
+    resp = session.request('POST', url + path, 
+                           data=json.dumps(data), 
+                           verify=True, 
+                           headers=headers)
+    return resp.json()
+
+
+def mkdirs(path, headers):
+    _data = {}
+    _data['path'] = path
+    return perform_query('/dbfs/mkdirs', headers=headers, data=_data)
+  
+
+def create(path, overwrite, headers):
+    _data = {}
+    _data['path'] = path
+    _data['overwrite'] = overwrite
+    return perform_query('/dbfs/create', headers=headers, data=_data)
+
+
+def add_block(handle, data, headers):
+    _data = {}
+    _data['handle'] = handle
+    _data['data'] = data
+    return perform_query('/dbfs/add-block', headers=headers, data=_data)
+
+
+def close(handle, headers):
+    _data = {}
+    _data['handle'] = handle
+    return perform_query('/dbfs/close', headers=headers, data=_data)
+
+
+def get_file_from_url(url, dbfs_path, overwrite, headers):
+    response = requests.get(url)
+    if response.status_code == 200:
+        content = response.content
+        handle = create(dbfs_path, overwrite, headers=headers)['handle']
+        print("Getting file: " + dbfs_path)
+        for i in range(0, len(content), 2**20):
+            add_block(handle, 
+                      base64.standard_b64encode(content[i:i+2**20]).decode(), 
+                      headers=headers)
+        close(handle, headers=headers)
+        print(f"File {dbfs_path} uploaded successfully.")
+    else:
+        print(f"Error downloading file from {url}. Status code: {response.status_code}")
+
+
+def extract(
+        url="""https://github.com/nogibjj/HugoHu-Project-3/blob/main/shopping_trends_updated.csv?raw=true""",
+        file_path=FILESTORE_PATH+"/shopping_trends.csv",
+        directory=FILESTORE_PATH,
+        overwrite=True
+):
+    """Extract a url to a file path"""
+    mkdirs(path=directory, headers=headers)
+    get_file_from_url(url, file_path, overwrite, headers=headers)
+
+    return file_path
+
+
+if __name__ == "__main__":
+    extract()
---
+++ ids706_python_template/query_visualize_data.py
@@ -0,0 +1,123 @@
+"""
+query and visualize data
+"""
+
+from pyspark.sql import SparkSession
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def query_transform1():
+    spark = SparkSession.builder.appName("Query").getOrCreate()
+    query = (
+        "SELECT"
+        "    CASE"
+        "        WHEN Age BETWEEN 0 AND 17 THEN '18-'"
+        "        WHEN Age BETWEEN 18 AND 24 THEN '18-24'"
+        "        WHEN Age BETWEEN 25 AND 30 THEN '25-30'"
+        "        WHEN Age BETWEEN 31 AND 45 THEN '31-45'"
+        "        WHEN Age BETWEEN 46 AND 60 THEN '46-60'"
+        "        ELSE '60+'"
+        "    END AS AgeGroup,"
+        "    Gender,"
+        "    Category,"
+        "    COUNT(*) AS NumberOfCustomers"
+        " FROM shopping_trends"
+        " GROUP BY AgeGroup, Gender, Category"
+        " ORDER BY AgeGroup, Gender, Category;"
+    )
+    query_result = spark.sql(query)
+    return query_result
+
+def query_transform2():
+    spark = SparkSession.builder.appName("Query").getOrCreate()
+    query = (
+        "SELECT"
+        "    Location,"
+        "    Season,"
+        "    AVG(Purchase_Amount__USD_) AS AvgPurchaseAmount"
+        " FROM shopping_trends"
+        " WHERE Subscription_Status = 'Yes'"
+        " GROUP BY Location, Season"
+        " ORDER BY Location, Season;"
+        )
+    query_result = spark.sql(query)
+    return query_result
+
+
+def query_transform3():
+    spark = SparkSession.builder.appName("Query").getOrCreate()
+    query = (
+        "SELECT"
+        "    Location,"
+        "    Season,"
+        "    AVG(Purchase_Amount__USD_) AS AvgPurchaseAmount"
+        " FROM shopping_trends"
+        " WHERE Subscription_Status = 'No'"
+        " GROUP BY Location, Season"
+        " ORDER BY Location, Season;"
+    )
+    query_result = spark.sql(query)
+    return query_result
+
+
+def plot_age_distribution():
+    df = query_transform1().toPandas()
+    if len(df) > 0:
+        print(f"Data validation passed. {len(df)} rows available.")
+    else:
+        print("No data available. Please investigate.")
+    # Plotting
+    plt.figure(figsize=(12, 8))
+    sns.barplot(x='AgeGroup', y='NumberOfCustomers', hue='Category', data=df, errorbar=None)
+    plt.title('Customer Age Distribution by Category and Gender')
+    plt.xlabel('Age Group')
+    plt.ylabel('Number of Customers')
+    plt.show()
+
+
+def plot_purchase_by_sub():
+    df = query_transform2().toPandas()
+    if len(df) > 0:
+        print(f"Data validation passed. {len(df)} rows available.")
+    else:
+        print("No data available. Please investigate.")
+
+    # Pivot the DataFrame for better visualization
+    pivot_df_subscribed = df.pivot_table(index='Location', columns='Season', values='AvgPurchaseAmount', aggfunc='mean')
+
+    # Plotting
+    plt.figure(figsize=(12, 8))
+    sns.heatmap(pivot_df_subscribed, annot=True, cmap='viridis', fmt=".2f", linewidths=.5)
+    plt.title('Average Purchase Amount by Location and Season (Subscribed Customers Only)')
+    plt.xlabel('Season')
+    plt.ylabel('Location')
+    plt.show()
+
+
+def plot_purchase_by_not_sub():
+    df = query_transform3().toPandas()
+    if len(df) > 0:
+        print(f"Data validation passed. {len(df)} rows available.")
+    else:
+        print("No data available. Please investigate.")
+
+    # Pivot the DataFrame for better visualization
+    pivot_df_subscribed = df.pivot_table(index='Location', columns='Season', values='AvgPurchaseAmount', aggfunc='mean')
+
+    # Plotting
+    plt.figure(figsize=(12, 8))
+    sns.heatmap(pivot_df_subscribed, annot=True, cmap='viridis', fmt=".2f", linewidths=.5)
+    plt.title('Average Purchase Amount by Location and Season (Subscribed Customers Only)')
+    plt.xlabel('Season')
+    plt.ylabel('Location')
+    plt.show()
+
+if __name__ == "__main__":
+    plot_age_distribution()
+    plot_purchase_by_sub()
+    plot_purchase_by_not_sub()
+
+
+
